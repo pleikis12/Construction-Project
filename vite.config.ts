39ffbd177b1e12cc043a14eebc82df399e82da36
Erikas <erikaspleikys@gmail.com>
2026-06-12 20:32:22 +0300

    feat: add sitemap generation

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite' 
+import Sitemap from 'vite-plugin-sitemap' // 1. Add this import
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    tailwindcss() 
+    tailwindcss(),
+    Sitemap({ 
+      hostname: 'https://uabvestar.com',
+    }),
   ],
   resolve: {
     alias: {
